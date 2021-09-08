5545d729fd5b158dbe2843fb7c7e92cf41769a89
Martín Javier Larré <75001420+Martin-J-Larre@users.noreply.github.com>
2021-09-07 22:34:39 -0400

    Primer commit-entrega clase-2-CREAR LA APP UTILIZANDO EL CLI

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+       <h2>Esta es la 5ta vez que empiezo este proyecto de nuevo</h2>
+       <h2>This is the fifth time I am starting this proyect again</h2>
+       <h2>This is my fifth time to restart this project</h2>
+       <h2>このプロジェクトを再スタートするのはこれで5回目です</h2>
+    </div>
+  );
+}
+
+export default App;
