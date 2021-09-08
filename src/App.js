8002f38872fac5c982845ea7f9799fcc46bc7533
Martín Javier Larré <75001420+Martin-J-Larre@users.noreply.github.com>
2021-09-08 00:03:04 -0400

    Entrega clase 3-MENÚ E-COMMERCE

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
+import NavBar from './components/NavBar/NavBar';
 
 function App() {
   return (
     <div>
+      <NavBar />
        <h2>Esta es la 5ta vez que empiezo este proyecto de nuevo</h2>
        <h2>This is the fifth time I am starting this proyect again</h2>
        <h2>This is my fifth time to restart this project</h2>
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from'./LOGO-2-.png';
+import './navbar.css';
+
+function NavBar() {
+    return (
+        <div className="header">
+            <nav className="header-nav">
+                <a className="nav-logo" href="#home" >
+                    <img src={logo} alt={"logo"}/>
+                </a>  
+               <ul>
+                    <li> <a href="#home">Home</a></li>
+                    <li> <a href="#aboutus">About us</a></li>
+                    <li> <a href="#products">Products</a></li>
+                    <li> <a href="#category">Category</a></li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default NavBar;
